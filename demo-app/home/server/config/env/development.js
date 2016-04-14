12e7fb7ef2c65ee7ddb5af65aa8f7d0e7e0e5772
Jess <maochengjie1988@gmail.com>
2016-04-14 17:50:35 +0800

    modify config

---
+++ demo-app/home/server/config/env/development.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jess on 16/4/14.
+ */
+
+'use strict';
+
+
+module.exports = {
+
+    redis : {
+        host : 'from home/config/env/development'
+    }
+};
---
+++ demo-app/home/server/config/mongo.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jess on 16/4/14.
+ */
+
+
+'use strict';
+
+
+module.exports = {
+
+    mongo_host : '127.0.0.0',
+    mongo_pwd : 'pwd'
+};
