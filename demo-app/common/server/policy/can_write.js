4c3be24e8c7218a445b3e08e6c1e4e06fb7a0652
Jess <maochengjie1988@gmail.com>
2016-04-22 15:44:32 +0800

    add http policy

--- demo-app/common/server/policy/can_write.js
+++ demo-app/common/server/policy/can_write.js
@@ -11,9 +11,20 @@ class CanWritePolicy extends weDemo.PolicyBase{
 
     execute(data){
 
-        grape.console.log(`can_write policy:` , data);
+        let query = this.http.query;
 
-        return Promise.resolve();
+        console.log( query );
+
+        if( query.ok === '1' ){
+
+            grape.console.log(`can_write policy:` , data);
+
+            return Promise.resolve();
+        }else{
+            return this.sendStatus(401);
+        }
+
+
     }
 }
 
