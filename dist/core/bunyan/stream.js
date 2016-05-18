40eb8349a8ef77f47193ac51940c1e8721b86f89
Jess <maochengjie1988@gmail.com>
2016-05-18 15:34:02 +0800

    modify bunyan dev stream

--- dist/core/bunyan/stream.js
+++ dist/core/bunyan/stream.js
@@ -5,21 +5,41 @@
 
 'use strict';
 
+var _assign = require('babel-runtime/core-js/object/assign');
+
+var _assign2 = _interopRequireDefault(_assign);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var EventEitter = require('events').EventEmitter;
 var util = require('util');
 
-function DevStream() {}
+function DevStream(options) {
+
+    this.options = (0, _assign2.default)({
+
+        showHidden: false,
+        depth: 4,
+        colors: false,
+        customInspect: false,
+        showProxy: false,
+        maxArrayLength: 100
+
+    }, options || {});
+}
 
 util.inherits(DevStream, EventEitter);
 
 DevStream.prototype.write = function (record) {
-    var str = util.inspect(record);
+    var str = util.inspect(record, this.options);
     console.log(str);
 };
 
 var singleton = {
 
-    dev: new DevStream()
+    dev: function dev(options) {
+        return new DevStream(options);
+    }
 
 };
 
